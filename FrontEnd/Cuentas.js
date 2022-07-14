f2e95c5a29aa8bbc5f334e5ab8f8d4aae71edb5d
joseQuixchan <64998165+joseQuixchan@users.noreply.github.com>
2022-07-13 21:38:26 -0600

    Query

--- FrontEnd/Cuentas.js
+++ FrontEnd/Cuentas.js
@@ -1,6 +1,7 @@
 var UrlApi = "https://localhost:44304/api/Cuenta";
 
 function AgregarCuenta(){
+ 
     var settings = {
         "url": UrlApi,
         "method": "POST",  
@@ -9,10 +10,10 @@ function AgregarCuenta(){
           "Content-Type": "application/json"
         },
         "data": JSON.stringify({
-            "id_cuenta": $("#id").val(),
+            "id_cuenta": $("id_cuenta").val(),
             "numero_cuenta": $("#numero_cuenta").val(),
             "descripcion": $("#descripcion").val(),
-            "fecha_ingreso": $("#fecha").val(),
+            "fecha_ingreso": $("#fecha_ingreso").val()
         }),
       };
     
--- QueryTablesAndScripts.sql
+++ QueryTablesAndScripts.sql
@@ -8,7 +8,7 @@ CREATE TABLE LibroDiario (
 )
 
 CREATE TABLE RegistroDiario (
-	id_registro int IDENTITY(1,1) PRIMARY KEY NOT NULL,
+	id_registro int PRIMARY KEY NOT NULL,
 	id_libro int NOT NULL,
 	id_cuenta int NOT NULL,
 	id_factura int NOT NULL,
@@ -17,7 +17,7 @@ CREATE TABLE RegistroDiario (
 	comentario nvarchar(50),
 	debe decimal(5,3),
 	haber decimal(5,3),
-	fecha_ingreso datetime Default GetDate(),
+	fecha_ingreso datetime,
 	subtotal decimal(5,2)
 
 	CONSTRAINT fk_id_libro FOREIGN KEY (id_libro) REFERENCES LibroDiario (id_libro),
@@ -26,10 +26,10 @@ CREATE TABLE RegistroDiario (
 )
 
 CREATE TABLE Cuenta (
-	id_cuenta int IDENTITY(1,1) PRIMARY KEY NOT NULL,
+	id_cuenta int PRIMARY KEY NOT NULL,
 	numero_cuenta nvarchar(15),
 	descripcion nvarchar(50),
-	fecha_ingreso datetime Default GetDate()
+	fecha_ingreso datetime
 )
 
 
@@ -53,3 +53,14 @@ CREATE TABLE DetalleFactura (
 
 SELECT A.fecha_ingreso, A.asieto, A.ap, B.numero_cuenta, B.descripcion, C.numero_factura, A.comentario, A.debe, A.haber, A.subtotal
 from RegistroDiario A, Cuenta B, Factura C WHERE A.id_cuenta = B.id_cuenta and A.id_cuenta = C.id_factura
+
+
+SELECT B., G.NOMBRE, F.
+FROM TIPO_INFORMACION A INNER JOIN FORMATO_MENSAJE B ON A.COD_TIPO_INFORMACION = B.COD_TIPO_INFORMACION
+INNER JOIN MENSAJE C ON C.COD_FORMATO = B.COD_FORMATO INNER JOIN PRODUCTO_PROYECTO D ON D.PROYECTO = C.PROYECTO
+INNER JOIN PRODUCTO F ON F.PRODUCTO = D.PRODUCTO INNER JOIN PROYECTO G ON G.PROYECTO = D.PROYECTO
+
+
+SELECT A.NOMBRE. C.PRODUCTO, C.DESCRIPCION
+FROM PROYECTO A INNER JOIN PRODUCTO_PROYECTO B ON A.PROYECTO = B.PROYECTO
+INNER JOIN PRODUCTO C ON B.PRODUCTO = C.PRODUCTO WHERE A.PROYECTO = 1 --PROYECTO PREMIA
